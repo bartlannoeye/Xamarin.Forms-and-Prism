3aeddcc2edf59c11dd161fe20cce1ebf201731e9
Bart Lannoeye <bart.lannoeye@gmail.com>
2017-06-17 23:21:51 +0200

    Step 1: MVVM - Adding ViewModelLocator

---
+++ PrismFormsAutofac/PrismFormsAutofac/PrismFormsAutofac/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace PrismFormsAutofac.ViewModels
+{
+    public class MainPageViewModel : BindableBase
+    {
+        public MainPageViewModel()
+        {
+            Title = "Hello from ViewModel";
+        }
+
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+    }
+}
