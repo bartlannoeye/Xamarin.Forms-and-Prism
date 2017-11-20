8163b5ff2b5eff56d1a6fa05e52afb2432f02da7
Unknown <bart.lannoeye@gmail.com>
2017-11-20 23:17:10 +0100

    Step 3a: Preparation for step 3

---
+++ PrismFormsAutofac/PrismFormsAutofac/PrismFormsAutofac/ViewModels/ViewAViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrismFormsAutofac.ViewModels
+{
+    public class ViewAViewModel : BindableBase
+    {
+        public ViewAViewModel()
+        {
+            NavigateCommand = new DelegateCommand(Navigate);
+        }
+
+        private string _title = "View A";
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public DelegateCommand NavigateCommand { get; private set; }
+
+        private void Navigate()
+        {
+        }
+    }
+}
---
+++ PrismFormsAutofac/PrismFormsAutofac/PrismFormsAutofac/ViewModels/ViewBViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace PrismFormsAutofac.ViewModels
+{
+    internal class ViewBViewModel : BindableBase
+    {
+        private readonly INavigationService _navigationService;
+
+        public ViewBViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            NavigateCommand = new DelegateCommand(Navigate);
+        }
+
+        private string _title = "View B";
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public DelegateCommand NavigateCommand { get; private set; }
+
+        private void Navigate()
+        {
+        }
+    }
+}
